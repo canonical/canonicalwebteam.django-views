95bc8b6e0eb1bc1dd24f022c0aa36b74ade096cf
Thomas Bille <thomas.bille@canonical.com>
2018-11-27 16:36:20 +0000

    Upgrade to version 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='canonicalwebteam.django_views',
-    version='1.2.0',
+    version='1.3.0',
     author='Canonical webteam',
     author_email='webteam@canonical.com',
     url='https://github.com/canonicalwebteam/django_views',
