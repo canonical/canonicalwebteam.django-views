f8af601488d9f3b0e59e927296300fd7bc981b37
Robin Winslow <robin@robinwinslow.co.uk>
2019-08-22 16:23:16 +0100

    Make it v1.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='canonicalwebteam.django_views',
-    version='1.4.0a1',
+    version='1.4.0',
     author='Canonical webteam',
     author_email='webteam@canonical.com',
     url='https://github.com/canonicalwebteam/django_views',
